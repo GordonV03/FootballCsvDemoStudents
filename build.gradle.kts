4f0e9ccc48bf4852716664719b5208ec97262981
NurlanFromOrsk <nurlantirzhanov@yandex.ru>
2024-09-22 14:08:45 +0500

    finish

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("com.github.javafaker:javafaker:1.0.2")
+    implementation("org.jfree:jfreechart:1.5.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ src/main/kotlin/resolver/IResolver.kt
@@ -0,0 +1,23 @@
+package resolver
+
+import model.ForwardFromTransferCost
+import model.Team
+
+interface IResolver {
+
+    // Выведите количество игроков, интересы которых не представляет агенство.
+    fun getCountWithoutAgency(): Int
+
+    // Выведите автора наибольшего числа голов из числа защитников и их количество.
+    fun getBestScorerDefender(): Pair<String, Int>
+
+    // Выведите русское название позиции самого дорогого немецкого игрока.
+    fun getTheExpensiveGermanPlayerPosition(): String
+
+    // Выберите команду с наибольшим числом удалений на одного игрока.
+    fun getTheRudestTeam(): Team
+
+    /* Выберите список футболистов для демонстрации */
+    /* зависимости количества забитых голов от трансферной стоимости для нападающих */
+    fun getForwardFromTransferCost(): List<ForwardFromTransferCost>
+}
